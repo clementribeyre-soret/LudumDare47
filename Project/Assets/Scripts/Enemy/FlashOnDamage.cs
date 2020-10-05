29a99dbea2f20595144d72172dcaf46547827bf2
Julien Revel <j.revel@dowino.com>
2020-10-05 18:59:14 +0200

    Added red flash on damage

---
+++ Project/Assets/Scripts/Enemy/FlashOnDamage.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class FlashOnDamage : MonoBehaviour
+{
+    public Color color;
+    private SpriteRenderer spriteRenderer;
+    private Health health;
+    private float intensity;
+    public float intensityDecreaseSpeed = 2;
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        health = GetComponentInParent<Health>();
+        health.onDamageDelegate += OnDamage;
+    }
+
+    public void OnDamage(Health health)
+    {
+        intensity = 1;
+    }
+
+    private void Update()
+    {
+        if(intensity > 0)
+        {
+            intensity -= Time.deltaTime;
+            spriteRenderer.color = Color.Lerp(Color.white, color, intensity);
+        }
+    }
+}
