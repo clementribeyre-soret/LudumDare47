5d624cf8e93ea0f233d75ddbaf2963fcb0846f32
Julien Revel <julien.revel@telecomnancy.net>
2020-10-05 20:20:17 +0200

    Destroy enemies when out of screen

--- Project/Assets/Scripts/Gameplay/DestroyOutOfScreen.cs
+++ Project/Assets/Scripts/Gameplay/DestroyOutOfScreen.cs
@@ -3,11 +3,12 @@ using UnityEngine;
 public class DestroyOutOfScreen : MonoBehaviour
 {
     public float minY = -6;
+    public float maxX = 10;
     
 
     public void Update()
     {
-        if(transform.position.y < minY)
+        if(transform.position.y < minY || transform.position.x < -maxX || transform.position.x > maxX)
         {
             Destroy(gameObject);
         }
